4c5cee55eec102703cdcec234c085c4e17bf9ee5
Michael McClenaghan <mike@sideline.ca>
2011-07-14 07:26:31 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ramirez::Application.config.secret_token = '7ca8beaf99030858f5d9a66b7bb4920e10ad5e40bbd2447904ca57496ffe9670182e5a1ca3c5675621d087542c9dd883def78b7d384967781bd5dac1586bbf01'
