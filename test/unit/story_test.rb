3b61047e839caa137d82a9e09e9981822f4acf9e
Michael McClenaghan <mike@sideline.ca>
2011-07-14 09:36:02 -0600

    Added extra validation tests for Story.

--- test/unit/story_test.rb
+++ test/unit/story_test.rb
@@ -4,5 +4,8 @@ class StoryTest < ActiveSupport::TestCase
   test "validates required fields" do
     story = Story.new
     assert_invalid(story)
+    assert(story.errors[:headline].any?, "Expected an error on validation")
+    assert(story.errors[:body].any?, "Expected an error on validation")
+    assert(story.errors[:feature].any?, "Expected an error on validation")
   end
 end
